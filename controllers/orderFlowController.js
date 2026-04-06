3c356a58a99142a018b9edb3231660e37ba1a6ca
naima-uddin <toktikproject@gmail.com>
2026-04-06 16:12:10 +0600

    Update orderFlowController.js

--- controllers/orderFlowController.js
+++ controllers/orderFlowController.js
@@ -67,6 +67,7 @@ const toLineItem = (item = {}) => {
     title: sanitizeText(String(item.title || item.name || "")),
     brand: sanitizeText(String(item.brand || "")),
     pattern: sanitizeText(String(item.pattern || "")),
+    size: sanitizeText(String(item.size || "")),
     ply: sanitizeText(String(item.ply || "")),
     image: item.image ? String(item.image) : "",
     quantity,
@@ -409,7 +410,7 @@ const generateInvoicePdfBuffer = (invoice) =>
     });
     y += 30;
 
-    const pCols = [0.16, 0.14, 0.1, 0.07, 0.08, 0.15, 0.2];
+    const pCols = [0.14, 0.12, 0.1, 0.1, 0.07, 0.08, 0.14, 0.15];
     const pWidths = pCols.map((ratio) => ratio * tableWidth);
     const pXs = pWidths.reduce((acc, w, i) => {
       acc.push((acc[i - 1] || tableX) + (i === 0 ? 0 : pWidths[i - 1]));
@@ -417,7 +418,7 @@ const generateInvoicePdfBuffer = (invoice) =>
     }, []);
 
     const headerHeight = 30;
-    const headers = ["Product Name", "Brand", "Pattern", "Ply", "QTY", "Unit Price\n(USD/Pics)", "Total Price (USD)"];
+    const headers = ["Product Name", "Brand", "Pattern", "Size", "Ply", "QTY", "Unit Price\n(USD/Pics)", "Total Price (USD)"];
     headers.forEach((header, index) => drawCell(pXs[index], y, pWidths[index], headerHeight, "", header));
     y += headerHeight;
 
@@ -429,6 +430,7 @@ const generateInvoicePdfBuffer = (invoice) =>
         sanitizeText(item.title || item.name || "-"),
         sanitizeText(String(item.brand || "-"), "-"),
         sanitizeText(String(item.pattern || "-"), "-"),
+        sanitizeText(String(item.size || "-"), "-"),
         sanitizeText(String(item.ply || "-"), "-"),
         String(item.quantity || 0),
         toPdfCurrency(item.unitPrice),
